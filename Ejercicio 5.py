b8354145895d197db232537acf862a261bfac4f1
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-02-27 18:46:25 +0100

    Ej 5

--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -1,8 +1,17 @@
 #Ejercicio resuelto 5: Búsqueda recursiva por dicotomía en una tabla ordenada
 Algoritmo dicotomía
 
-Entrada
+ENTRADA
     tabla:TABLA[T -> COMPARABLE]
     t:T
 
 Resultado:ENTERO
+
+
+PRECONDICION
+    t ≠ NULO
+    tabla ≠ NULO
+
+    está_ordenada_asc(tabla) #tabla ordenada ascendentemente
+
+    tabla[índice_min(tabla)] ≤ t ≤ tabla[índice_max(tabla)] #t dentro de la tabla
