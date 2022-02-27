1890aa3bba78c24bbc99e135a27eb1d5904ec759
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-02-27 19:18:07 +0100

    Ej 6

--- Ejercicio 6.py
+++ Ejercicio 6.py
@@ -1,1 +1,27 @@
 #Ejercicio 6: Reconocer un palíndromo
+Programa palíndromo
+
+
+# Esperar la escritura en teclado de la primera frase a analizar
+leer(TECLADO)
+frase ← copia_buffer(TECLADO)
+
+hasta que frase = CADENA_VACIA repetir
+    # Analizar la frase introducida y mostrar la respuesta
+    escribir(PANTALLA, frase)
+
+    si
+        palindromo(frase)
+    entonces
+        escribir(PANTALLA, ’ es un palíndromo.')
+    si no
+        escribir(PANTALLA, / no es un palíndromo./)
+    fin si
+
+    # Esperar la escritura en teclado de frase siguiente a analizar
+    leer(TECLADO)
+    frase ← copia_buffer(TECLADO)
+
+fin repetir
+
+fin programa palindromo
